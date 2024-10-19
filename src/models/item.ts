4101964c55c085cffb8ab73f7b6838101d946aad
deepthi1203 <deepthi12.k@gmail.com>
2024-10-18 20:39:37 -0400

    insert sample data into db

---
+++ src/models/item.ts
@@ -0,0 +1,15 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface Item extends Document {
+  name: string;
+  description: string;
+}
+
+const itemSchema = new Schema<Item>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const ItemModel = model<Item>('Item', itemSchema);
+
+export default ItemModel;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
+import ItemModel from 'models/item';
 
 dotenv.config();
 
@@ -21,6 +22,11 @@ const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@codetest.jy
 mongoose.connect(MONGO_URL)
   .then(() => {
     console.log('MongoDB connected');
+
+    const newItem = new ItemModel({ name: 'Deepthi', description: 'Rio Tinto sample project' });
+    newItem.save()
+      .then(item => console.log('item:', item))
+      .catch(error => console.error('error:', error));
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
